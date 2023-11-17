ce7faec414d6d1ee33fd0d7d255f580447b2f174
Eir Nym <485399+eirnym@users.noreply.github.com>
2023-11-18 00:01:19 +0100

    Cleanup settings and urls from unused features

--- src/chameleon/api/urls.py
+++ src/chameleon/api/urls.py
@@ -2,7 +2,4 @@ from django.urls import include, re_path
 
 from chameleon.api import project
 
-
-urlpatterns = [
-    re_path("^project", include(project)),
-]
+urlpatterns = (re_path("^project", include(project)),)
