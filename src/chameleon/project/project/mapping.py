aa292291af565043641ac369abd12d60379641e8
Eir Nym <485399+eirnym@users.noreply.github.com>
2023-11-19 18:30:45 +0100

    Split project API module to make it more readable

---
+++ src/chameleon/project/project/mapping.py
@@ -0,0 +1,31 @@
+from chameleon.step.impl import registry
+from chameleon.project.project.models import ChameleonProject
+
+
+registry.register_simple_mapping_dict(
+    type_id="project",
+    action_id="create",
+    target_object_type=ChameleonProject,
+    fields=("title", "description", "description_markup"),
+)
+
+registry.register_simple_mapping_object(
+    type_id="project",
+    action_id="get",
+    target_object_type=dict,
+    fields=("id", "title", "description", "description_markup"),
+)
+
+registry.register_simple_mapping_object(
+    type_id="project",
+    action_id="list",
+    target_object_type=dict,
+    fields=("id", "title"),
+)
+
+registry.register_simple_mapping_dict(
+    type_id="project",
+    action_id="update",
+    target_object_type=dict,
+    fields=("title", "description", "description_markup"),
+)
