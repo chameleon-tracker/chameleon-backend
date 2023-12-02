b49d8e451718b6b45d256e032229403bf4a82b8e
Eir Nym <485399+eirnym@users.noreply.github.com>
2023-12-02 11:39:03 +0100

    Autoname app

--- src/chameleon/common/chameleon_app.py
+++ src/chameleon/common/chameleon_app.py
@@ -1,44 +1,11 @@
-from django.apps import AppConfig
+from chameleon.common.app.autoname import AutonameAppConfig
 
 __all__ = ["ChameleonAppConfig"]
 
 
-class ChameleonAppConfig(AppConfig):
-    """Chameleon project basic application config.
-
-    This class initializes fields `name`, `label` and `default` to make them
-    compatible with AppConfig and automatic registration.
-
-    NOTE: name, label and default class-level values will be overriten
-
-    Basic usage:
-
-    >>> class MyAppConfig(ChameleonAppConfig):
-    ...     ...  # no need to define those parameters by default
-
-    In case of a requirement to override these parameters use as below:
-
-    >>> class MyAppConfig(ChameleonAppConfig, default=False):
-    ...     ...  # no need to define those parameters by default
-
-    """
-
-    def __init_subclass__(cls, /, name=None, label=None, default=True, **kwargs):
-        super().__init_subclass__(**kwargs)
-
-        if not name:
-            name = cls.__module__
-
-            if "." in name:
-                name = name[: name.rfind(".")]
-
-        if not label:
-            label = name.replace(".", "_")
-
-        setattr(cls, "name", name)
-        setattr(cls, "label", label)
-        setattr(cls, "default", default)
+def chameleon_label(name: str) -> str | None:
+    return name.replace(".", "_")
 
 
-# cleanup namespace
-del AppConfig
+class ChameleonAppConfig(AutonameAppConfig, default=False, label=chameleon_label):
+    ...
