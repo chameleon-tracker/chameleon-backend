d103667c9cc0d7b6f464fea2db74c9bb293c6ebf
Eir Nym <485399+eirnym@users.noreply.github.com>
2024-01-16 19:27:25 +0100

    Extract common API methods

--- chameleon/api/project.py
+++ chameleon/api/project.py
@@ -2,22 +2,20 @@ import logging
 
 from django.urls import re_path
 
-from chameleon.api.chameleon import chameleon_validation_error_handler
+from chameleon.api import chameleon
 from chameleon.project.project import api
 from chameleon.project.ticket import api as ticket_api
-from chameleon.step.framework import steps_django as django
 
 logger = logging.getLogger(__name__)
 
-processor_create = django.django_json_steps(
+processor_create = chameleon.chameleon_json_steps(
     type_id="project",
     action_id_input="create",
     action_id_output="get",
     business=api.project_create_fun,
-    exception_handler={"validate_input": chameleon_validation_error_handler},
 )
 
-processor_list = django.django_json_steps(
+processor_list = chameleon.chameleon_json_steps(
     type_id="project",
     map_input=None,
     action_id_output="get",
@@ -26,22 +24,21 @@ processor_list = django.django_json_steps(
 )
 
 # path variables: `project_id` - project public ID
-processor_get = django.django_json_steps(
+processor_get = chameleon.chameleon_json_steps(
     business=api.project_get_fun,
     type_id="project",
     map_input=None,
     action_id_output="get",
 )
 
-processor_update = django.django_json_steps(
+processor_update = chameleon.chameleon_json_steps(
     type_id="project",
     action_id_input="update",
     action_id_output="get",
     business=api.project_update_fun,
-    exception_handler={"validate_input": chameleon_validation_error_handler},
 )
 
-processor_history = django.django_json_steps(
+processor_history = chameleon.chameleon_json_steps(
     type_id="project",
     map_input=None,
     action_id_output="history",
@@ -49,15 +46,14 @@ processor_history = django.django_json_steps(
     business=api.project_history,
 )
 
-processor_ticket_create = django.django_json_steps(
+processor_ticket_create = chameleon.chameleon_json_steps(
     type_id="ticket",
     action_id_input="create",
     action_id_output="get",
     business=ticket_api.ticket_create_fun,
-    exception_handler={"validate_input": chameleon_validation_error_handler},
 )
 
-processor_ticket_list = django.django_json_steps(
+processor_ticket_list = chameleon.chameleon_json_steps(
     type_id="ticket",
     map_input=None,
     action_id_output="get",
@@ -65,37 +61,23 @@ processor_ticket_list = django.django_json_steps(
     business=ticket_api.ticket_list_fun,
 )
 
-error_status_to_http = {1: 400}
 urlpatterns = (
     re_path(
         "^$",
-        django.method_dispatcher(
-            get=processor_list,
-            post=processor_create,
-            error_status_to_http=error_status_to_http,
-        ),
+        chameleon.method_dispatcher(get=processor_list, post=processor_create),
     ),
     re_path(
         "^/(?P<project_id>[a-zA-Z0-9_-]+)$",
-        django.method_dispatcher(
-            get=processor_get,
-            post=processor_update,
-            error_status_to_http=error_status_to_http,
-        ),
+        chameleon.method_dispatcher(get=processor_get, post=processor_update),
     ),
     re_path(
         "^/(?P<project_id>[a-zA-Z0-9_-]+)/history$",
-        django.method_dispatcher(
-            get=processor_history,
-            error_status_to_http=error_status_to_http,
-        ),
+        chameleon.method_dispatcher(get=processor_history),
     ),
     re_path(
         "^/(?P<project_id>[a-zA-Z0-9_-]+)/ticket$",
-        django.method_dispatcher(
-            get=processor_ticket_list,
-            post=processor_ticket_create,
-            error_status_to_http=error_status_to_http,
+        chameleon.method_dispatcher(
+            get=processor_ticket_list, post=processor_ticket_create
         ),
     ),
 )
--- chameleon/api/ticket.py
+++ chameleon/api/ticket.py
@@ -2,29 +2,27 @@ import logging
 
 from django.urls import re_path
 
-from chameleon.api.chameleon import chameleon_validation_error_handler
+from chameleon.api import chameleon
 from chameleon.project.ticket import api
-from chameleon.step.framework import steps_django as django
 
 logger = logging.getLogger(__name__)
 
 # path variables: `ticket_id` - ticket public ID
-processor_get = django.django_json_steps(
+processor_get = chameleon.chameleon_json_steps(
     business=api.ticket_get_fun,
     type_id="ticket",
     map_input=None,
     action_id_output="get",
 )
 
-processor_update = django.django_json_steps(
+processor_update = chameleon.chameleon_json_steps(
     type_id="ticket",
     action_id_input="update",
     action_id_output="get",
     business=api.ticket_update_fun,
-    exception_handler={"validate_input": chameleon_validation_error_handler},
 )
 
-processor_history = django.django_json_steps(
+processor_history = chameleon.chameleon_json_steps(
     type_id="ticket",
     map_input=None,
     action_id_output="history",
@@ -36,17 +34,10 @@ error_status_to_http = {1: 400}
 urlpatterns = (
     re_path(
         "^/(?P<ticket_id>[a-zA-Z0-9_-]+)$",
-        django.method_dispatcher(
-            get=processor_get,
-            post=processor_update,
-            error_status_to_http=error_status_to_http,
-        ),
+        chameleon.method_dispatcher(get=processor_get, post=processor_update),
     ),
     re_path(
         "^/(?P<ticket_id>[a-zA-Z0-9_-]+)/history$",
-        django.method_dispatcher(
-            get=processor_history,
-            error_status_to_http=error_status_to_http,
-        ),
+        chameleon.method_dispatcher(get=processor_history),
     ),
 )
